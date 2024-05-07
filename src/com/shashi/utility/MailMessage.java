228f3d191ce39c66774c156f575f82cb63ff74c1
Himanshu <99754294+Kazuto335@users.noreply.github.com>
2024-05-07 16:39:32 +0530

    MailMessage.java

--- src/com/shashi/utility/MailMessage.java
+++ src/com/shashi/utility/MailMessage.java
@@ -7,8 +7,8 @@ public class MailMessage {
 		String recipient = emailId;
 		String subject = "Registration Successfull";
 		String htmlTextMessage = "" + "<html>" + "<body>"
-				+ "<h2 style='color:green;'>Welcome to Ellison Electronics</h2>" + "" + "Hi " + name + ","
-				+ "<br><br>Thanks for singing up with Ellison Electronics.<br>"
+				+ "<h2 style='color:green;'>Welcome to Shoplift Trends</h2>" + "" + "Hi " + name + ","
+				+ "<br><br>Thanks for singing up with Shoplift Trends.<br>"
 				+ "We are glad that you choose us. We invite you to check out our latest collection of new electonics appliances."
 				+ "<br>We are providing upto 60% OFF on most of the electronic gadgets. So please visit our site and explore the collections."
 				+ "<br><br>Our Online electronics is growing in a larger amount these days and we are in high demand so we thanks all of you for "
@@ -27,9 +27,9 @@ public class MailMessage {
 
 	public static void transactionSuccess(String recipientEmail, String name, String transId, double transAmount) {
 		String recipient = recipientEmail;
-		String subject = "Order Placed at Ellison Electronics";
+		String subject = "Order Placed at Shoplift Trends";
 		String htmlTextMessage = "<html>" + "  <body>" + "    <p>" + "      Hey " + name + ",<br/><br/>"
-				+ "      We are glad that you shop with Ellison Electronics!" + "      <br/><br/>"
+				+ "      We are glad that you shop with Shoplift Trends!" + "      <br/><br/>"
 				+ "      Your order has been placed successfully and under process to be shipped."
 				+ "<br/><h6>Please Note that this is a demo projet Email and you have not made any real transaction with us till now!</h6>"
 				+ "      <br/>" + "      Here is Your Transaction Details:<br/>" + "      <br/>"
@@ -37,7 +37,7 @@ public class MailMessage {
 				+ "      <font style=\"color:green;font-weight:bold;\">" + transId + "</font><br/>" + "      <br/>"
 				+ "      <font style=\"color:red;font-weight:bold;\">Amount Paid:</font> <font style=\"color:green;font-weight:bold;\">"
 				+ transAmount + "</font>" + "      <br/><br/>" + "      Thanks for shopping with us!<br/><br/>"
-				+ "      Come Shop Again! <br/<br/> <font style=\"color:green;font-weight:bold;\">Ellison Electronics.</font>"
+				+ "      Come Shop Again! <br/<br/> <font style=\"color:green;font-weight:bold;\">Shoplift Trends.</font>"
 				+ "    </p>" + "    " + "  </body>" + "</html>";
 
 		try {
@@ -49,9 +49,9 @@ public class MailMessage {
 
 	public static void orderShipped(String recipientEmail, String name, String transId, double transAmount) {
 		String recipient = recipientEmail;
-		String subject = "Hurray!!, Your Order has been Shipped from Ellison Electronics";
+		String subject = "Hurray!!, Your Order has been Shipped from Shoplift Trends";
 		String htmlTextMessage = "<html>" + "  <body>" + "    <p>" + "      Hey " + name + ",<br/><br/>"
-				+ "      We are glad that you shop with Ellison Electronics!" + "      <br/><br/>"
+				+ "      We are glad that you shop with Shoplift Trends!" + "      <br/><br/>"
 				+ "      Your order has been shipped successfully and on the way to be delivered."
 				+ "<br/><h6>Please Note that this is a demo projet Email and you have not made any real transaction with us till now!</h6>"
 				+ "      <br/>" + "      Here is Your Transaction Details:<br/>" + "      <br/>"
@@ -59,7 +59,7 @@ public class MailMessage {
 				+ "      <font style=\"color:green;font-weight:bold;\">" + transId + "</font><br/>" + "      <br/>"
 				+ "      <font style=\"color:red;font-weight:bold;\">Amount Paid:</font> <font style=\"color:green;font-weight:bold;\">"
 				+ transAmount + "</font>" + "      <br/><br/>" + "      Thanks for shopping with us!<br/><br/>"
-				+ "      Come Shop Again! <br/<br/> <font style=\"color:green;font-weight:bold;\">Ellison Electronics.</font>"
+				+ "      Come Shop Again! <br/<br/> <font style=\"color:green;font-weight:bold;\">Shoplift Trends.</font>"
 				+ "    </p>" + "    " + "  </body>" + "</html>";
 
 		try {
@@ -71,9 +71,9 @@ public class MailMessage {
 
 	public static void productAvailableNow(String recipientEmail, String name, String prodName, String prodId) {
 		String recipient = recipientEmail;
-		String subject = "Product " + prodName + " is Now Available at Ellison Electronics";
+		String subject = "Product " + prodName + " is Now Available at Shoplift Trends";
 		String htmlTextMessage = "<html>" + "  <body>" + "    <p>" + "      Hey " + name + ",<br/><br/>"
-				+ "      We are glad that you shop with Ellison Electronics!" + "      <br/><br/>"
+				+ "      We are glad that you shop with Shoplift Trends!" + "      <br/><br/>"
 				+ "      As per your recent browsing history, we seen that you were searching for an item that was not available in sufficient amount"
 				+ " at that time. <br/><br/>"
 				+ "We are glad to say that the product named <font style=\"color:green;font-weight:bold;\">" + prodName
@@ -86,7 +86,7 @@ public class MailMessage {
 				+ prodId + " " + "      </font><br/>" + "      <br/>"
 				+ "      <font style=\"color:red;font-weight:bold;\">Product Name: </font> <font style=\"color:green;font-weight:bold;\">"
 				+ prodName + "</font>" + "      <br/><br/>" + "      Thanks for shopping with us!<br/><br/>"
-				+ "      Come Shop Again! <br/<br/><br/> <font style=\"color:green;font-weight:bold;\">Ellison Electronics.</font>"
+				+ "      Come Shop Again! <br/<br/><br/> <font style=\"color:green;font-weight:bold;\">Shoplift Trends.</font>"
 				+ "    </p>" + "    " + "  </body>" + "</html>";
 
 		try {
